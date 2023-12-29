f5efdb7a1f756db339929a69960660053ddbf558
Arul-95 <skas95@gmail.com>
2023-12-29 15:55:37 +0530

    Pull

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,13 @@
+package org.bank;
+
+public class BankInfo {
+public void saving (long amount) {
+	System.out.println("Saving amount is "+amount);
+}
+public void fixed (long amount) {
+	System.out.println("Fixed amount is "+amount);
+}
+public void deposit (long amount) {
+	System.out.println("Deposit Amount is "+amount);
+}
+}
